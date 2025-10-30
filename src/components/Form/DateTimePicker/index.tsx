ad65e29f55d156a241a5eeda1f5f190477b742e6
AshileySabah <ashileysabrina2002@gmail.com>
2025-10-30 10:07:28 -0300

    feat: DateTimePicker

--- src/components/Form/DateTimePicker/index.tsx
+++ src/components/Form/DateTimePicker/index.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-import { Controller } from "react-hook-form";
-
-import { InputBaseComponentProps, TextField } from "@mui/material";
+import { InputBaseComponentProps } from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import {
   DateTimePicker as DateTimePickerMui,
   DateTimePickerProps as DateTimePickerPropsMui,
 } from "@mui/x-date-pickers/DateTimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import ptLocale from "date-fns/locale/pt-BR";
-
+import { ptBR } from "date-fns/locale/pt-BR";
+import React from "react";
+import { Controller } from "react-hook-form";
 import { IFormProps } from "../@types/form";
 
 interface IDatePickerProps extends IFormProps {
   inputProps?: InputBaseComponentProps;
-  DateTimePickerProps?: Partial<DateTimePickerPropsMui<any, any>>;
+  DateTimePickerProps?: Partial<DateTimePickerPropsMui>;
 }
 
 export const DateTimePicker: React.FC<IDatePickerProps> = ({
@@ -32,31 +30,27 @@ export const DateTimePicker: React.FC<IDatePickerProps> = ({
       field: { ref, value, onChange, onBlur },
       fieldState: { error },
     }) => (
-      <LocalizationProvider
-        dateAdapter={AdapterDateFns}
-        locale={ptLocale}
-        adapterLocale={ptLocale}
-      >
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
         <DateTimePickerMui
           readOnly={!!inputProps?.readOnly}
           inputRef={ref}
           label={label}
           value={value}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              fullWidth
-              color="primary"
-              size="small"
-              error={!!error}
-              helperText={error ? error.message : ""}
-              onBlur={onBlur}
-            />
-          )}
+          slotProps={{
+            textField: {
+              fullWidth: true,
+              color: "primary",
+              size: "small",
+              inputRef: ref,
+              onBlur,
+              inputProps: inputProps as any,
+              error: !!error,
+              helperText: error?.message ?? "",
+            } as any,
+          }}
           {...DateTimePickerProps}
           onChange={(e) => {
             onChange(e);
-            DateTimePickerProps?.onChange?.(e);
           }}
         />
       </LocalizationProvider>
