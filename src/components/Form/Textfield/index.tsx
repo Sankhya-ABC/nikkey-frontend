f265326310ec6df5b92698d3d54ed4e4774674d3
AshileySabah <ashileysabrina2002@gmail.com>
2025-10-30 10:17:01 -0300

    feat: TextField

---
+++ src/components/Form/Textfield/index.tsx
@@ -0,0 +1,58 @@
+import {
+  TextField as MuiTextField,
+  TextFieldProps as MuiTextFieldProps,
+} from "@mui/material";
+import React from "react";
+import { Controller } from "react-hook-form";
+import { IFormProps } from "../@types/form";
+
+interface ITextFieldProps extends IFormProps {
+  TextFieldProps?: Partial<MuiTextFieldProps>;
+  type?: "text" | "number" | "email" | "password" | "tel" | "url";
+  multiline?: boolean;
+  rows?: number;
+  maxLength?: number;
+}
+
+export const TextField: React.FC<ITextFieldProps> = ({
+  name,
+  control,
+  label,
+  TextFieldProps,
+  type = "text",
+  multiline = false,
+  rows = 4,
+  maxLength,
+}) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({
+        field: { ref, value, onChange, onBlur },
+        fieldState: { error },
+      }) => (
+        <MuiTextField
+          {...TextFieldProps}
+          fullWidth
+          color="primary"
+          size="small"
+          type={type}
+          label={label}
+          value={value ?? ""}
+          inputRef={ref}
+          onBlur={onBlur}
+          onChange={onChange}
+          error={!!error}
+          helperText={error?.message ?? ""}
+          multiline={multiline}
+          rows={multiline ? rows : undefined}
+          inputProps={{
+            maxLength,
+            ...TextFieldProps?.inputProps,
+          }}
+        />
+      )}
+    />
+  );
+};
