fa59f3e5954b99b614a32d5fdc274f330e1887e8
AshileySabah <ashileysabrina2002@gmail.com>
2025-11-03 15:38:15 -0300

    refact: OrdemServico - types

--- src/pages/OrdemDeServico/type.ts
+++ src/pages/OrdemDeServico/type.ts
@@ -1,23 +1,117 @@
-export interface OrdemDeServico {
-  cliente: string;
-  tecnico: string;
-  dataVisita: Date | null;
-  visitasPendentes: string;
-  horaInicio: Date | null;
-  horaFim: Date | null;
-  responsavel: string;
-  cargoResponsavel: string;
-  servicoRealizado: boolean;
-  motivoNaoRealizacao: string;
-  evidenciasPragas: boolean;
-  pragas: string | number;
-  revisaoEquipamentos: boolean;
-  oQueFoiVisualizado: string | number;
+export enum ComoEncontrado {
+  INDIVIDUOS_VIVOS = "INDIVIDUOS_VIVOS",
+  INDIVIDUOS_MORTOS = "INDIVIDUOS_MORTOS",
+  FRAGMENTOS = "FRAGMENTOS",
+}
+
+export interface Praga {
+  idPraga: number;
+  comoEncontrado: ComoEncontrado;
+  ondeEncontrado: string;
   quantidade: string;
-  areaEncontrado: string;
-  consumoProdutos: boolean;
-  naoConformidades: boolean;
-  uploadEvidencias: boolean;
+}
+
+export interface QuantidadeIdentificacao {
+  quantidade: number | string;
+  identificacao: string[];
+}
+
+export enum TipoContagem {
+  TOTAL = "TOTAL",
+  ESPECIE = "ESPECIE",
+}
+
+export interface Consumo {
+  idPraga: number;
+  idProduto: number;
+  lote: string;
+  validade: Date | null;
+  idEquipamento: number;
+  quantidade: number | string;
+}
+
+// seções da tela
+export interface InformacoesGerais {
+  idCliente: number;
+  idTecnico: number;
+  data: {
+    data: Date | null;
+    horaInicio: Date | null;
+    horaFinal: Date | null;
+  };
+  responsavel: {
+    nome: string;
+    cargo: string;
+  };
+  flagPossuiVisitaPendente: boolean;
+}
+
+export interface InformacoesBasicasServico {
+  flagServicoRealizado: boolean;
+  motivoNaoRealizacao: string;
+}
+
+export interface PragasEncontradas {
+  flagEvidenciasOuFocosPragas: boolean;
+  pragas: Praga[];
+}
+
+export interface Equipamentos {
+  flagRevisaoEquipamentos: boolean;
+
+  iscagem: {
+    flag: boolean;
+    quantidade: number | string;
+    mofoDeterioracao: QuantidadeIdentificacao;
+    roido: QuantidadeIdentificacao;
+    obstruidoQuebradoExtraviado: QuantidadeIdentificacao;
+  };
+
+  placaColaArmadilhaMecanica: {
+    flag: boolean;
+    quantidade: number | string;
+    sujeiraDeterioracao: QuantidadeIdentificacao;
+    roedorAderido: QuantidadeIdentificacao;
+    obstruidoQuebradoExtraviado: QuantidadeIdentificacao;
+  };
+
+  armadilhaLuminosa: {
+    flag: boolean;
+    flagClienteExigeContagemInsetosPorArmadilha: boolean;
+    tipoContagem: TipoContagem;
+    contagem:
+      | (QuantidadeIdentificacao & { idPraga: number }[])
+      | QuantidadeIdentificacao[];
+  };
+
+  armadilhaFeromonio: {
+    flag: boolean;
+    quantidade: number | string;
+    guachon: QuantidadeIdentificacao;
+    bioSerrico: QuantidadeIdentificacao;
+  };
+}
+
+export interface ConsumoProdutos {
+  flagConsumoProdutos: boolean;
+  consumo: Consumo[];
+}
+
+export interface NaoConformidade {
+  areaLocal: string;
+  naoConformidade: string;
+  acaoSugerida: string;
+}
+
+export interface InformacoesAdicionais {
+  naoConformidades: {
+    flag: boolean;
+    naoConformidades: NaoConformidade[];
+  };
   observacoes: string;
-  revisaoIscagem: boolean;
+}
+
+export interface UploadEvidencias {
+  flagUploadEvidencias: boolean;
+  uploads: File[];
 }
