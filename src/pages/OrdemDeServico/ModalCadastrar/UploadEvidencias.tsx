7ec66d190a294e6975500cf2b54133d81081e3c2
AshileySabah <ashileysabrina2002@gmail.com>
2025-11-04 00:53:30 -0300

    refact: OrdemServico - UploadEvidencias

---
+++ src/pages/OrdemDeServico/ModalCadastrar/UploadEvidencias.tsx
@@ -0,0 +1,38 @@
+import { Box, Grid, Typography } from "@mui/material";
+import { useFormContext } from "react-hook-form";
+import { Switch } from "../../../components/Form/Switch";
+
+export const UploadEvidencias = () => {
+  const { control, watch } = useFormContext();
+
+  const flagUploadEvidencias = watch("flagUploadEvidencias");
+
+  return (
+    <Box>
+      <Typography variant="h6" color="primary">
+        Upload de Evidências
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item size={{ xs: 12 }}>
+          <Switch
+            control={control}
+            name="flagUploadEvidencias"
+            label="Upload de evidências?"
+          />
+        </Grid>
+
+        {flagUploadEvidencias && (
+          <Grid item size={{ xs: 12 }}>
+            <input
+              type="file"
+              multiple
+              onChange={(e) => {
+                const files = Array.from(e.target.files || []);
+              }}
+            />
+          </Grid>
+        )}
+      </Grid>
+    </Box>
+  );
+};
