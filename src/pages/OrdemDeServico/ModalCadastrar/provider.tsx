3605b2878fbd42485aea07071a0c6061bae47c6d
AshileySabah <ashileysabrina2002@gmail.com>
2025-11-04 00:53:44 -0300

    refact: OrdemServico - provider

---
+++ src/pages/OrdemDeServico/ModalCadastrar/provider.tsx
@@ -0,0 +1,25 @@
+export const listClientes = [
+  { id: 1, descricao: "Cliente A" },
+  { id: 2, descricao: "Cliente B" },
+];
+
+export const listTecnicos = [
+  { id: 1, descricao: "Técnico 1" },
+  { id: 2, descricao: "Técnico 2" },
+];
+
+export const listPragas = [
+  { id: 1, descricao: "Barata" },
+  { id: 2, descricao: "Rato" },
+  { id: 3, descricao: "Formiga" },
+];
+
+export const listProdutos = [
+  { id: 1, descricao: "Produto A" },
+  { id: 2, descricao: "Produto B" },
+];
+
+export const listEquipamentos = [
+  { id: 1, descricao: "Equipamento 1" },
+  { id: 2, descricao: "Equipamento 2" },
+];
