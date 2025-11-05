2a875359ef80a7a67e54f4f0ea7cd9339f7fbb48
AshileySabah <ashileysabrina2002@gmail.com>
2025-11-05 01:22:21 -0300

    refact: OrdemServico - QuantidadeIdentificacao

---
+++ src/pages/OrdemDeServico/ModalCadastrar/QuantidadeIdentificacao.tsx
@@ -0,0 +1,79 @@
+import {
+  Box,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { FieldPath, useFormContext } from "react-hook-form";
+import { TextField } from "../../../components/Form/Textfield";
+import { OrdemServico } from "../type";
+
+interface QuantidadeIdentificacaoProps {
+  title: string;
+  property: string;
+}
+
+export const QuantidadeIdentificacao: React.FC<
+  QuantidadeIdentificacaoProps
+> = ({ title, property }) => {
+  const { control, watch } = useFormContext<OrdemServico>();
+
+  const iscagemMofoDeterioracaoQuantidade = Number(
+    watch(`${property}.quantidade` as FieldPath<OrdemServico>),
+  );
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Typography variant="subtitle1" gutterBottom>
+        {title}
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item size={{ xs: 12, md: 6 }}>
+          <TextField
+            control={control}
+            name={`${property}.quantidade`}
+            label="Quantidade"
+            type="number"
+          />
+        </Grid>
+        {iscagemMofoDeterioracaoQuantidade > 0 && (
+          <Grid item size={{ xs: 12 }}>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Identificação</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {Array.from({
+                    length: iscagemMofoDeterioracaoQuantidade,
+                  })?.map((_data, index) => {
+                    return (
+                      <TableRow>
+                        <TableCell sx={{ width: "25%" }}>
+                          <TextField
+                            control={control}
+                            name={`${property}.identificacao.${index}`}
+                            label={`${index + 1}`}
+                          />
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        )}
+      </Grid>
+    </Box>
+  );
+};
