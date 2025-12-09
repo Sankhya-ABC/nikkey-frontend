7b93f960e51189d58827f08b3718463ce688ecee
AshileySabah <ashileysabrina2002@gmail.com>
2025-12-09 10:04:54 -0300

    feat: Service - types

--- src/services/types.ts
+++ src/services/types.ts
@@ -4,3 +4,19 @@ export enum CRUDType {
   UPDATE = "Editar",
   DELETE = "Deletar",
 }
+
+export interface ParamsForPagination {
+  per_page: number;
+  current_page: number;
+  search?: string;
+}
+
+export interface GetAllPaginated<T> {
+  data: T[];
+  meta: {
+    current_page: number;
+    per_page: number;
+    total: number;
+    last_page: number;
+  };
+}
