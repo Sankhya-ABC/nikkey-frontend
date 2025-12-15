62ee7684143e930d2acfe547e95b83238bf36ba1
AshileySabah <ashileysabrina2002@gmail.com>
2025-12-15 14:55:09 -0300

    lint: fix

---
+++ eslint.config.mjs
@@ -0,0 +1,137 @@
+import { defineConfig } from "eslint/config";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11Y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  ...tseslint.configs.recommended,
+
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended",
+      ),
+    ),
+
+    plugins: {
+      react: fixupPluginRules(react),
+      "react-hooks": fixupPluginRules(reactHooks),
+      "jsx-a11y": fixupPluginRules(jsxA11Y),
+      prettier,
+      import: importPlugin,
+    },
+
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+
+      globals: {
+        ...globals.browser,
+      },
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "no-case-declarations": "off",
+      "react/prop-types": "off",
+
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^[A-Z_]",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+          reportUsedIgnorePattern: false,
+        },
+      ],
+
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "@components/**",
+              group: "internal",
+            },
+            {
+              pattern: "@hooks/**",
+              group: "internal",
+            },
+            {
+              pattern: "@pages/**",
+              group: "internal",
+            },
+            {
+              pattern: "@services/**",
+              group: "internal",
+            },
+            {
+              pattern: "@utils/**",
+              group: "internal",
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+]);
